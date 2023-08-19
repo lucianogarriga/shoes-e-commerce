f7c35f1a5261edaf2c0c0123dd54e67c3b9acc1f
Luciano Garriga <lucianomgarriga@gmail.com>
2023-08-19 14:27:20 -0300

    Add new styles and images to Item Cards

--- src/components/Pure/Item/Item.jsx
+++ src/components/Pure/Item/Item.jsx
@@ -2,25 +2,26 @@ import React from "react";
 import ItemCount from "../ItemCount/ItemCount";
 
 const Item = ({ item }) => {
-
   const onAdd = (count) => {
     console.log(`Compraste ${count} unidades!`);
   };
 
-  return ( 
-          <div className="item">
-          <a href="" className="card">
-              <img src={item.pictureUrl} />
-            <div className="card-body">
-              <h5 className="card-title">{item.name}</h5>
-              <p className="card-text">{item.description}</p>
-              <div className="item-content">
-                <h3>${item.price}</h3>
-                <ItemCount stock={5} initial={1} onAdd={onAdd} />
-              </div>
-            </div>
-            </a>
-          </div> 
+  return (
+    <div className="item">
+      <a href="" className="card">
+        <div> 
+          <img src={item.pictureUrl} className="card-picture" />
+        </div>
+        <div className="card-body">
+          <div className="item-content">
+          <p className="card-title">{item.name}</p>
+          {/* <p className="card-text">{item.description}</p> */}
+            <h3>${item.price}</h3>
+            <ItemCount stock={5} initial={1} onAdd={onAdd} />
+          </div>
+        </div>
+      </a>
+    </div>
   );
 };
 

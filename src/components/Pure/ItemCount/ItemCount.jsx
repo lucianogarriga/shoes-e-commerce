1e26820a16c012484a9428f70f1aeb74c76b05d4
Luciano Garriga <lucianomgarriga@gmail.com>
2023-08-24 00:21:42 -0300

    Add styles & elements to ItemDetail Card

--- src/components/Pure/ItemCount/ItemCount.jsx
+++ src/components/Pure/ItemCount/ItemCount.jsx
@@ -18,21 +18,43 @@ const ItemCount = ({ stock, initial, onAdd }) => {
 
   return (
     <div className="item-count-container">
-      <div className="item-count-products">
-        <button type="button" className="btn btn-outline-dark" disabled={count < initial} onClick={handleSub}>
-          -
-        </button>
-        <span className="count">{count}</span>
-        <button type="button" className="btn btn-outline-dark"  disabled={count >= stock} onClick={handleSum}>
-          +
-        </button>
+      <div className="item-count-stock">
+        <p className="item-stock-text">Stock disponible</p>
+        <div className="item-count-quantity">
+        <p className="count">Cantidad: <span className="item-quantity-selected">{count} unidad</span></p>
+        <p className="item-quantity-available">({stock} disponibles) </p>
+        </div>
+
       </div>
-      <div>
+      {/* <div className="item-count-products">
+        <button
+          type="button"
+          className="btn btn-outline-dark"
+          disabled={count < initial}
+          onClick={handleSub}
+        >   -
+        </button>
+        <button
+          type="button"
+          className="btn btn-outline-dark"
+          disabled={count >= stock}
+          onClick={handleSum}
+        > +
+        </button>
+      </div> */}
+      <div className="item-count-cart-buttons">
+        <button
+          disabled={count === 0 && count < stock}
+          onClick={() => onAdd(count)}
+          className="btn btn-primary"
+        > 
+          Comprar ahora
+        </button>
         <button
           disabled={count === 0 && count < stock}
           onClick={() => onAdd(count)}
           className="btn btn-outline-primary"
-        >
+        > 
           Agregar al carrito
         </button>
       </div>

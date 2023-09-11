74d7c683606ed21146e5389a01889d2feaa5e639
Luciano Garriga <lucianomgarriga@gmail.com>
2023-09-11 20:21:51 -0300

    Add info into Seller component

--- src/components/Pure/ItemDetail/ItemSeller.jsx
+++ src/components/Pure/ItemDetail/ItemSeller.jsx
@@ -7,6 +7,24 @@ const ItemSeller = () => {
             <h2 className="item-title-seller">
                 Información sobre el vendedor
               </h2>
+              <div className='item-seller-info'>
+                <div className='item-seller-status-info'>
+                    <i></i>
+                    <div>
+                        <p>MercadoLíder Platinum</p>
+                        <p>¡Es uno de los mejores del sitio!</p>
+                    </div>
+                </div>
+                <ul></ul>
+                <div className='item-seller-reputation-info'>
+                    <ul className='item-seller-list-description'>
+                        <li className='item-seller-description'>Ventas en los últimos 60 días</li>
+                        <li className='item-seller-description'>Brinda buena atención</li>
+                        <li className='item-seller-description'>Despacha sus productos a tiempo</li>
+                    </ul>
+                </div>
+              </div>
+              <a className='item-seller-link' href=''>Ver más datos de este vendedor</a>
         </div>
     );
 }

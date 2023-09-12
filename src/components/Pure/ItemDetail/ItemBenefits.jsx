01c251ef5ca33210aac57b436ee3dcfb3d0b9331
Luciano Garriga <lucianomgarriga@gmail.com>
2023-09-12 12:44:06 -0300

    Rename css files - Add styles in Seller Info - Add format in Seller-Item-Description

--- src/components/Pure/ItemDetail/ItemBenefits.jsx
+++ src/components/Pure/ItemDetail/ItemBenefits.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./ItemBenefits.css";
+import "./itemBenefits.css";
 
 const ItemBenefits = () => {
   return (
--- src/components/Pure/ItemDetail/ItemSeller.jsx
+++ src/components/Pure/ItemDetail/ItemSeller.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./ItemSeller.css";
+import "./itemSeller.css";
 
 const ItemSeller = () => {
     return (
@@ -11,14 +11,17 @@ const ItemSeller = () => {
                 <div className='item-seller-status-info'>
                     <i></i>
                     <div>
-                        <p>MercadoLíder Platinum</p>
-                        <p>¡Es uno de los mejores del sitio!</p>
+                        <p className='item-status-title'>MercadoLíder Platinum</p>
+                        <p className='item-status-subtitle'>¡Es uno de los mejores del sitio!</p>
                     </div>
                 </div>
                 <ul></ul>
                 <div className='item-seller-reputation-info'>
                     <ul className='item-seller-list-description'>
-                        <li className='item-seller-description'>Ventas en los últimos 60 días</li>
+                        <li className='item-seller-description'>
+                        <strong>+1000</strong>
+                        <p>Ventas en los últimos 60 días</p> 
+                        </li>
                         <li className='item-seller-description'>Brinda buena atención</li>
                         <li className='item-seller-description'>Despacha sus productos a tiempo</li>
                     </ul>

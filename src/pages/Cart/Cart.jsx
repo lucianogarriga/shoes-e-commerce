26f9c4ccedc0de4063ec92cbfb924cdb5c94236d
Luciano Garriga <lucianomgarriga@gmail.com>
2023-09-18 20:36:34 -0300

    Mod cart page structure

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import "./cart.css";
 
 const Cart = () => {
-    return (
-        <div>
-          <h1 >Cart</h1>  
+  return (
+    <div>
+      <h1>Cart</h1>
+      <div className="cards-container">
+        <div className="cards-list">
+          Descripción vendedor y producto
         </div>
-    );
-}
+        <div className="price-box-container">
+
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Cart;

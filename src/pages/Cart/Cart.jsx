c06d1e08e27514ecc0149579ef5e003c7f0bb416
Luciano Garriga <lucianomgarriga@gmail.com>
2023-09-18 20:55:11 -0300

    Create mobile structure (divs/span/button) - Add basics margin & padding

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -3,14 +3,24 @@ import "./cart.css";
 
 const Cart = () => {
   return (
-    <div>
-      <h1>Cart</h1>
+    <div className="cards-double-scroll">
+      <h1 className="cart-title">Cart</h1>
       <div className="cards-container">
         <div className="cards-list">
-          Descripción vendedor y producto
+          <div className="card-wrapper">
+            <div className="andes-card__header">Header</div>
+            <div className="item-row">Item description</div>
+            <div className="andes-card__footer">Footer</div>
+          </div>
+ 
         </div>
         <div className="price-box-container">
-
+          <div className="expandable-footer"></div>
+          <button className="to-checkout">
+            <span className="andes-button__content">
+              <span className="andes-button__text">Continuar compra</span>
+            </span>
+          </button>
         </div>
       </div>
     </div>

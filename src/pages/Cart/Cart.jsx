162838ffd78d1ccbe37bf5fc2463c23b14d1c96a
Luciano Garriga <lucianomgarriga@gmail.com>
2023-10-02 20:30:46 -0300

    Items to buy details (left container) in ItemCart view Finished (PC & Mobile)

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import "./cart.css";
+import "./cart.css"; 
+import logo from '../../../public/cart-img.png';
 import { useCartContext } from "../../context/CartProvider";
 import { Link } from "react-router-dom";
 import ItemCart from "../../components/Pure/ItemCart/ItemCart";
@@ -14,7 +15,7 @@ const Cart = () => {
           <div className="cards-container">
             <div className="cards-list">
               <div className="state-container">
-                <svg></svg>
+                <img src={logo}/>
                 <span className="bf-ui-rich-text">¡Empieza un carrito de compras!</span>
                 <span className="bf-ui-rich-text text-gray">Sumá productos y conseguí envío gratis.</span>
                 <div className="bf-ui-button-container">
@@ -49,12 +50,13 @@ const Cart = () => {
               {cart.map((product) => (
                 <ItemCart key={product.id} product={product} />
               ))}
-              <p>Precio total: {totalPrice()}</p>
+             
             </div>
           </div>
 
           <div className="price-box-container">
             <div className="expandable-footer"></div>
+            <p>Precio total: {totalPrice()}</p>
             <button className="to-checkout">
               <span className="andes-button__content">
                 <span className="andes-button__text">Continuar compra</span>

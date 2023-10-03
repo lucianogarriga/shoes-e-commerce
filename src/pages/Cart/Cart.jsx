de975686d59790c60b9361a761d56fa132a21738
Luciano Garriga <lucianomgarriga@gmail.com>
2023-10-03 16:06:53 -0300

    Add link to checkout section in 'Continue' button - Cart Page

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import "./cart.css"; 
-import logo from '../../../public/cart-img.png';
+import "./cart.css";
+import logo from "../../../public/cart-img.png";
 import { useCartContext } from "../../context/CartProvider";
 import { Link } from "react-router-dom";
 import ItemCart from "../../components/Pure/ItemCart/ItemCart";
@@ -15,18 +15,23 @@ const Cart = () => {
           <div className="cards-container">
             <div className="cards-list">
               <div className="state-container">
-                <img src={logo}/>
-                <span className="bf-ui-rich-text">¡Empieza un carrito de compras!</span>
-                <span className="bf-ui-rich-text text-gray">Sumá productos y conseguí envío gratis.</span>
+                <img src={logo} />
+                <span className="bf-ui-rich-text">
+                  ¡Empieza un carrito de compras!
+                </span>
+                <span className="bf-ui-rich-text text-gray">
+                  Sumá productos y conseguí envío gratis.
+                </span>
                 <div className="bf-ui-button-container">
-                  <button className="andes-button bf-ui-button"> 
-                      <Link to="/">
-                        <span className="andes-button__content">Descubrir productos
-                        </span> 
-                      </Link>
+                  <button className="andes-button bf-ui-button">
+                    <Link to="/">
+                      <span className="andes-button__content">
+                        Descubrir productos
+                      </span>
+                    </Link>
                   </button>
                 </div>
-              </div> 
+              </div>
             </div>
 
             {/* <div className="price-box-container">
@@ -50,7 +55,6 @@ const Cart = () => {
               {cart.map((product) => (
                 <ItemCart key={product.id} product={product} />
               ))}
-             
             </div>
           </div>
 
@@ -63,7 +67,7 @@ const Cart = () => {
                       <div className="ticket-row__left">
                         <div className="andes-tooltip">
                           <h5 className="ticket-row__left--text">
-                          <span className="rich-text">Productos</span>
+                            <span className="rich-text">Productos</span>
                           </h5>
                         </div>
                       </div>
@@ -77,7 +81,7 @@ const Cart = () => {
                       <div className="ticket-row__left">
                         <div className="andes-tooltip">
                           <h5 className="ticket-row__left--text">
-                          <span className="rich-text">Envío</span>
+                            <span className="rich-text">Envío</span>
                           </h5>
                         </div>
                       </div>
@@ -93,28 +97,31 @@ const Cart = () => {
                       <div>
                         <div className="andes-tooltip">
                           <div className="ticket-row__left">
-                          <h5 className="ticket-row__left--text">
-                            <span className="rich-text rich-text--bold">Total</span>
+                            <h5 className="ticket-row__left--text">
+                              <span className="rich-text rich-text--bold">
+                                Total
+                              </span>
                             </h5>
                           </div>
                         </div>
                       </div>
                       <div className="ticket-row__right">
-                          <h6 className="ticket-row__right--text">
-                            <span className="offers-total">$ {totalPrice()}</span>
-                          </h6>
-                        </div>
-                    </div> 
+                        <h6 className="ticket-row__right--text">
+                          <span className="offers-total">$ {totalPrice()}</span>
+                        </h6>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-            
-            <button className="andes-button to-checkout bf-ui-button">
-              <span className="andes-button__content">
-                <span className="andes-button__text">Continuar compra</span>
-              </span>
-            </button>
+            <Link to="/checkout">
+              <button className="andes-button to-checkout bf-ui-button">
+                <span className="andes-button__content">
+                  <span className="andes-button__text">Continuar compra</span>
+                </span>
+              </button>
+            </Link>
           </div>
         </div>
       </div>

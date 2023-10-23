ac735a07b1e7cffe5628fd01b32e06d90021d769
Luciano Garriga <lucianomgarriga@gmail.com>
2023-10-23 19:42:01 -0300

    Install firebase - Initialize firebase / config.js file

---
+++ frontend/src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBh5MVrGcqRU0peH3K1Fu2GZL87Zhcbj6Q",
+  authDomain: "clon-meli.firebaseapp.com",
+  projectId: "clon-meli",
+  storageBucket: "clon-meli.appspot.com",
+  messagingSenderId: "529473258550",
+  appId: "1:529473258550:web:ee91ac0f8b33cf24cf4497"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
